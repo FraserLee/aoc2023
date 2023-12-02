fd26c585dc4cf7e9e2797f524a2c08a0cd573424
Fraser <tomatoesneverlie@gmail.com>
2023-12-02 01:14:07 -0500

    port 2a to haskell too

--- 2/2.hs
+++ 2/2.hs
@@ -6,10 +6,11 @@ import Data.List.Split
 main :: IO ()
 main = do
     contents <- readFile "input.txt"
-    putStrLn $ "Part B: " ++ show ( sum $ map ((\games -> 
-           let (min_r, min_g, min_b) = foldl (\(r, g, b) game -> 
+
+    putStrLn $ "Part B: " ++ show ( sum $ map ((\games ->
+           let (min_r, min_g, min_b) = foldl (\(r, g, b) game ->
                        let moves = splitOn ", " game in
-                       foldl (\(r, g, b) move -> 
+                       foldl (\(r, g, b) move ->
                            let [n, colour] = splitOn " " move in
                            if      colour == "red"   then (max r (read n), g, b)
                            else if colour == "green" then (r, max g (read n), b)
@@ -19,3 +20,19 @@ main = do
                    ) (0, 0, 0) games
            in min_r * min_g * min_b
        ) . splitOn "; " . last . splitOn ": ") $ lines contents)
+
+    putStrLn $ "Part A: " ++ show ( sum $ zipWith (\i games ->
+           let (min_r, min_g, min_b) = foldl (\(r, g, b) game ->
+                       let moves = splitOn ", " game in
+                       foldl (\(r, g, b) move ->
+                           let [n, colour] = splitOn " " move in
+                           if      colour == "red"   then (max r (read n), g, b)
+                           else if colour == "green" then (r, max g (read n), b)
+                           else if colour == "blue"  then (r, g, max b (read n))
+                           else error "Invalid colour"
+                       ) (r, g, b) moves
+                   ) (0, 0, 0) games
+           in if min_r < 13 && min_g < 14 && min_b < 15 
+              then i 
+              else 0
+       ) [1..] $ map (splitOn "; " . last . splitOn ": ") $ lines contents)
