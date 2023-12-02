89de721115d922fe006b2c0d784cebdd352c9601
Fraser <tomatoesneverlie@gmail.com>
2023-12-02 00:57:18 -0500

    refine 2b haskell

--- 2/2.hs
+++ 2/2.hs
@@ -6,17 +6,16 @@ import Data.List.Split
 main :: IO ()
 main = do
     contents <- readFile "input.txt"
-    print $ sum $ map (\games -> 
-                let (min_r, min_g, min_b) = foldl (\(r, g, b) game -> 
-                            let moves = splitOn ", " game in
-                            foldl (\(r, g, b) move -> 
-                                let [n, colour] = splitOn " " move in
-                                if      colour == "red"   then (max r (read n :: Int), g, b)
-                                else if colour == "green" then (r, max g (read n :: Int), b)
-                                else if colour == "blue"  then (r, g, max b (read n :: Int))
-                                else error "Invalid colour"
-                            ) (r, g, b) moves
-                        ) (0, 0, 0) games in min_r * min_g * min_b
-            ) $ map (\line -> splitOn "; " (last (splitOn ": " line))) $ lines contents
-
-
+    putStrLn $ "Part B: " ++ show ( sum $ map ((\games -> 
+           let (min_r, min_g, min_b) = foldl (\(r, g, b) game -> 
+                       let moves = splitOn ", " game in
+                       foldl (\(r, g, b) move -> 
+                           let [n, colour] = splitOn " " move in
+                           if      colour == "red"   then (max r (read n), g, b)
+                           else if colour == "green" then (r, max g (read n), b)
+                           else if colour == "blue"  then (r, g, max b (read n))
+                           else error "Invalid colour"
+                       ) (r, g, b) moves
+                   ) (0, 0, 0) games
+           in min_r * min_g * min_b
+       ) . splitOn "; " . last . splitOn ": ") $ lines contents)
